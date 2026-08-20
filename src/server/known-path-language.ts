b8bdc5c4d0db325f6f81a3108232dec9c1d2dcb5
Hugo Josefson <hugo@josefson.org>
2026-08-20 15:14:34 +0200

    feat: detect languages from filenames and shebangs

---
+++ src/server/known-path-language.ts
@@ -0,0 +1,41 @@
+import { getLanguageByFileName } from "file-lang-map";
+
+const linguistLanguages: Readonly<Record<string, string>> = {
+  "c": "c",
+  "c#": "csharp",
+  "c++": "cpp",
+  "css": "css",
+  "editorconfig": "ini",
+  "git config": "ini",
+  "go": "go",
+  "html": "markup",
+  "ini": "ini",
+  "java": "java",
+  "javascript": "javascript",
+  "json": "json",
+  "json5": "json5",
+  "jsx": "jsx",
+  "markdown": "markdown",
+  "python": "python",
+  "rust": "rust",
+  "shell": "bash",
+  "svg": "markup",
+  "toml": "toml",
+  "tsx": "tsx",
+  "typescript": "typescript",
+  "xml": "markup",
+  "yaml": "yaml",
+};
+
+export function knownPathLanguage(path: string): string | undefined {
+  const normalizedPath = path.replaceAll("\\", "/").replace(/\/{2,}/g, "/");
+  const comparablePath = normalizedPath.toLowerCase();
+  if (
+    comparablePath.endsWith("/.git/config") || comparablePath === ".git/config"
+  ) {
+    return "ini";
+  }
+  return getLanguageByFileName(normalizedPath)
+    ?.map((language) => linguistLanguages[language.toLowerCase()])
+    .find((language) => language !== undefined);
+}
